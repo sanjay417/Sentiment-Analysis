566f56564556c8272c903614b559efb63057c194
Sanjay Mirani <miranisanjay417@gmail.com>
2020-11-04 19:40:08 -0500

    modified

--- multimedia_movie.py
+++ multimedia_movie.py
@@ -49,4 +49,31 @@ classifier = NaiveBayesClassifier.train(train_set)
 accuracy = classify.accuracy(classifier, test_set)
 print(accuracy) 
 
+"""
+os.chdir('C:/Users/miran/Desktop/multimedia_project/aclImdb/test/pos')
 
+for filename in os.listdir(os.getcwd()):
+   with open(os.path.join(os.getcwd(), filename), 'r') as f:
+
+        custom_review = f.read()
+        custom_review_tokens = word_tokenize(custom_review)
+        custom_review_set = bag_of_words(custom_review_tokens)
+        print (classifier.classify(custom_review_set)) 
+
+        prob_result = classifier.prob_classify(custom_review_set)
+        print (prob_result) 
+        print (prob_result.max())
+        print (prob_result.prob("neg")) 
+        print (prob_result.prob("pos")) """
+
+custom_review = "It was a wonderful and amazing movie. I loved it. Best direction, good acting."
+custom_review_tokens = word_tokenize(custom_review)
+custom_review_set = bag_of_words(custom_review_tokens)
+ 
+print (classifier.classify(custom_review_set)) 
+ 
+prob_result = classifier.prob_classify(custom_review_set)
+print (prob_result) 
+print (prob_result.max()) 
+print (prob_result.prob("neg"))
+print (prob_result.prob("pos")) 
